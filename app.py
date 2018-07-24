528c5fec736a64bb56943166ff8081e79a42d382
clip <binderclip2014@gmail.com>
2018-07-24 16:42:06 +0800

    support bot send msg

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, render_template
 
 import bot
 
@@ -40,6 +40,7 @@ def _event_handler(event_type, slack_event):
         if item_user_id and reaction == 'poultry_leg':      # test
             item = json.dumps(slack_event["event"]["item"], separators=(',', ':'))
             print(f'{user_id} ({reaction}) > {item_user_id} @({item})')
+            pyBot.notify_being_added_poultry_leg(user_id=user_id, item_user_id=item_user_id)
         return make_response("reaction logged", 200,)
 
     # ============= Event Type Not Found! ============= #
@@ -80,6 +81,34 @@ def hears():
 
     return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids you're looking for.",
                          404, {"X-Slack-No-Retry": 1})
+
+
+@app.route("/install", methods=["GET"])
+def pre_install():
+    """This route renders the installation page with 'Add to Slack' button."""
+    # Since we've set the client ID and scope on our Bot object, we can change
+    # them more easily while we're developing our app.
+    client_id = pyBot.oauth["client_id"]
+    scope = pyBot.oauth["scope"]
+    # Our template is using the Jinja templating language to dynamically pass
+    # our client id and scope
+    return render_template("install.html", client_id=client_id, scope=scope)
+
+
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    """
+    This route is called by Slack after the user installs our app. It will
+    exchange the temporary authorization code Slack sends for an OAuth token
+    which we'll save on the bot object to use later.
+    To let the user know what's happened it will also render a thank you page.
+    """
+    # Let's grab that temporary authorization code Slack's sent us from
+    # the request's parameters.
+    code_arg = request.args.get('code')
+    # The bot's auth method to handles exchanging the code for an OAuth token
+    pyBot.auth(code_arg)
+    return render_template("thanks.html")
 
 
 def main():
