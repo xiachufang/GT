03ecf1cae3461fd0bb2ca080978ef6c2e7979207
gfreezy <gfreezy@gmail.com>
2018-07-28 22:19:23 +0800

    track local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,9 @@
+# import logging
+# LOGLEVEL = logging.DEBUG
+SLACK_API_TOKEN = 'xxx'
+PLUGINS = [
+    'machine.plugins.builtin.general.PingPongPlugin',
+    'machine.plugins.builtin.general.HelloPlugin',
+    'machine.plugins.builtin.debug.EventLoggerPlugin',
+    'gt.plugins.chickens.ChickensPlugin',
+]
